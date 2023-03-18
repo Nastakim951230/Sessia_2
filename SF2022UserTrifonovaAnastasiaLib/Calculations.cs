db0983b20ff99c0c6b53358103d8d4a58771829b
Настя Ким <nastytrifono03@gmail.com>
2023-03-18 12:05:55 +0300

    dll

--- SF2022UserTrifonovaAnastasiaLib/Calculations.cs
+++ SF2022UserTrifonovaAnastasiaLib/Calculations.cs
@@ -11,34 +11,60 @@ namespace SF2022UserTrifonovaAnastasiaLib
 
         public string[] AvailablePeriods(TimeSpan[] startTimes,int[] durations,TimeSpan beginWorkingTime, TimeSpan endWorkingTime,int consultationTime)
         {
-            TimeSpan time = endWorkingTime - beginWorkingTime;
-            TimeSpan consultation=new TimeSpan(0,0,consultationTime);
-            TimeSpan zero=new TimeSpan(0,0,0);
-            
-            int count=0;
-            int countStartTime=startTimes.Length;
-            while (time != zero)
+            TimeSpan time = beginWorkingTime;
+            TimeSpan consultation = new TimeSpan(0, consultationTime, 0);
+
+
+            int count = 0;
+            int countStartTime = durations.Length;
+            while (time != endWorkingTime)
             {
-                time = time - consultation;
+                time = time + consultation;
                 count++;
             }
+
             string[] periods = new string[count];
             int j = 0;
-            for (int i=0;i<count;i++)
+            int k = 0;
+            for (int i = 0; i <= count; i++)
             {
-                if (j != countStartTime)
+                if (beginWorkingTime != endWorkingTime)
                 {
-                    TimeSpan duration = new TimeSpan(0, 0, durations[j]);
-                    if (beginWorkingTime == startTimes[j])
+                    if (j != countStartTime)
                     {
-                        beginWorkingTime = beginWorkingTime + duration;
-                        j++;
+                        TimeSpan duration = new TimeSpan(0, durations[j], 0);
+                        if (beginWorkingTime == startTimes[j])
+                        {
+                            beginWorkingTime = beginWorkingTime + duration;
+                            j++;
+                        }
+                        else if (beginWorkingTime + consultation == beginWorkingTime)
+                        {
+                            beginWorkingTime = beginWorkingTime + duration;
+                            j++;
+                        }
+                        else if (beginWorkingTime + consultation > startTimes[j] && startTimes[j] >= beginWorkingTime)
+                        {
+                            beginWorkingTime = startTimes[j] + duration;
+                            j++;
+                        }
+
+
+                        else
+                        {
+                            periods[k] = string.Format("{0:hh\\:mm}-{1:hh\\:mm} ", beginWorkingTime, beginWorkingTime + consultation);
+
+                            beginWorkingTime = beginWorkingTime + consultation;
+                            k++;
+
+                        }
                     }
                     else
                     {
-                        periods[i] = string.Format("{0:hh\\:mm}-{1:hh\\:mm}", beginWorkingTime, beginWorkingTime + consultation);
-                        beginWorkingTime=beginWorkingTime + consultation;
-                        
+
+                        periods[k] = string.Format("{0:hh\\:mm}-{1:hh\\:mm} ", beginWorkingTime, beginWorkingTime + consultation);
+                        beginWorkingTime = beginWorkingTime + consultation;
+                        k++;
                     }
                 }
             }
