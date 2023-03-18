45add853b1fb8adb46bc80ae54a48c901e672bf5
Настя Ким <nastytrifono03@gmail.com>
2023-03-18 10:51:11 +0300

    Добавьте файлы проекта.

---
+++ SF2022UserTrifonovaAnastasiaLib/Calculations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022UserTrifonovaAnastasiaLib
+{
+    public class Calculations
+    {
+
+        public string[] AvailablePeriods(TimeSpan[] startTimes,int[] durations,TimeSpan beginWorkingTime, TimeSpan endWorkingTime,int consultationTime)
+        {
+            TimeSpan time = endWorkingTime - beginWorkingTime;
+            TimeSpan consultation=new TimeSpan(0,0,consultationTime);
+            TimeSpan zero=new TimeSpan(0,0,0);
+            
+            int count=0;
+            int countStartTime=startTimes.Length;
+            while (time != zero)
+            {
+                time = time - consultation;
+                count++;
+            }
+            string[] periods = new string[count];
+            int j = 0;
+            for (int i=0;i<count;i++)
+            {
+                if (j != countStartTime)
+                {
+                    TimeSpan duration = new TimeSpan(0, 0, durations[j]);
+                    if (beginWorkingTime == startTimes[j])
+                    {
+                        beginWorkingTime = beginWorkingTime + duration;
+                        j++;
+                    }
+                    else
+                    {
+                        periods[i] = string.Format("{0:hh\\:mm}-{1:hh\\:mm}", beginWorkingTime, beginWorkingTime + consultation);
+                        beginWorkingTime=beginWorkingTime + consultation;
+                        
+                    }
+                }
+            }
+            return periods;
+        }
+    }
+}
---
+++ SF2022UserTrifonovaAnastasiaLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SF2022UserTrifonovaAnastasiaLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SF2022UserTrifonovaAnastasiaLib")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("79cd6b68-0e15-41eb-ad34-3f80f84ddc77")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
